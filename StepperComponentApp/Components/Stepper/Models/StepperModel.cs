ec1ba5266f2171a5d43467e0346fa052f9504a8d
sufi-webverse <sufiyanbudye7@gmail.com>
2025-02-05 21:04:00 +0530

    blazor stepper component created

---
+++ StepperComponentApp/Components/Stepper/Models/StepperModel.cs
@@ -0,0 +1,10 @@
+﻿namespace StepperComponentApp.Components.Stepper.Models
+{
+    public class StepperStepModel
+    {
+        public int SequenceNo { get; set; }
+        public string Title { get; set; }
+
+
+    }
+}
